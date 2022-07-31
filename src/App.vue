19556c17af36f383b9e737d4c8181aabd4385584
Macbook Pro <macbookpro@Macbooks-MBP.bbrouter>
2022-07-31 22:39:22 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<div class="bg-slate-200">
+  <nav class="relative flex container mx-auto p-3">
+    <div class="flex items-center justify-between">
+      <div class="font-bold space-x-10 md:flex ">
+        <router-link to="/" class="hover:text-blue-500 hover:underline"
+          >Home</router-link
+        >
+        <router-link
+          to="/about"
+          class="hover:text-blue-500 hover:underline"
+          >About</router-link
+        >
+
+        <router-link
+          to="/project"
+          class="hover:text-blue-500 hover:underline"
+          >Project</router-link
+        >
+        <router-link
+          to="/about"
+          class="hover:text-blue-500 hover:underline"
+          >Blog</router-link
+        >
+      </div>
+    </div>
+
+       <div class="flex flex-row justify-end flex-auto">
+      <router-link to="/login" class="justify-end text-white font-bold rounded-full self-baseline px-6 py-2 bg-slate-500 hover:bg-slate-300 hover:text-black">Log In</router-link>
+      <img src="../src/assets/profile.jpeg" alt="Image Profile" class="self-center w-10 h-10 rounded-full ml-5"/>
+   </div>
   </nav>
-  <router-view/>
+  </div>
+  <router-view />
 </template>
+
+<script>
+
+export default {
+
+};
+</script>
 
 <style>
 #app {
@@ -15,7 +50,7 @@
   color: #2c3e50;
 }
 
-nav {
+/* nav {
   padding: 30px;
 }
 
@@ -26,5 +61,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import About from '../views/AboutView.vue'
+import LogInView from '../views/LogInView.vue'
+import ProjectView from '../views/ProjectView.vue'
 
 const routes = [
   {
@@ -10,10 +13,17 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: About
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LogInView
+  },
+  {
+    path: '/project',
+    name: 'project',
+    component: ProjectView
   }
 ]
 
